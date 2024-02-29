ef1fd55c5758d195265a5696d0cefd0c301284c7
Stu <kewalaka@gmail.com>
2024-03-01 10:49:27 +1300

    feat: linux support for docker & python, and fixing variables to align with AVM spec (#12)

--- locals.tf
+++ locals.tf
@@ -1,5 +1,13 @@
 # TODO: insert locals here.
 locals {
+  managed_identities = {
+    system_assigned_user_assigned = (var.managed_identities.system_assigned || length(var.managed_identities.user_assigned_resource_ids) > 0) ? {
+      this = {
+        type                       = var.managed_identities.value.system_assigned && length(var.managed_identities.user_assigned_resource_ids) > 0 ? "SystemAssigned, UserAssigned" : length(var.managed_identities.user_assigned_resource_ids) > 0 ? "UserAssigned" : "SystemAssigned"
+        user_assigned_resource_ids = var.managed_identities.user_assigned_resource_ids
+      }
+    } : {}
+  }
   pe_role_assignments = { for ra in flatten([
     for pe_k, pe_v in var.private_endpoints : [
       for rk, rv in pe_v.role_assignments : {
