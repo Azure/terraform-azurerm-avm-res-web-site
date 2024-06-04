bf9ca0f429a4040dee71412f6d0e2368952f05fe
Donovan McCoy <144926888+donovm4@users.noreply.github.com>
2024-06-04 18:16:55 -0400

    ftp fixes, linux new storage fixes (#75)

--- examples/basic_auth/main.tf
+++ examples/basic_auth/main.tf
@@ -21,6 +21,7 @@ provider "azurerm" {
   }
 }
 
+
 ## Section to provide a random Azure region for the resource group
 # This allows us to randomize the region for the resource group.
 module "regions" {
@@ -41,20 +42,40 @@ module "naming" {
   version = ">= 0.3.0"
 }
 
+# This is required for resource modules
 resource "azurerm_resource_group" "example" {
   location = local.azure_regions[random_integer.region_index.result]
   name     = module.naming.resource_group.name_unique
 }
 
+/*
+module "avm_res_storage_storageaccount" {
+  source  = "Azure/avm-res-storage-storageaccount/azurerm"
+  version = "0.1.1"
+
+  enable_telemetry = false
+  name                          = module.naming.storage_account.name_unique
+  resource_group_name           = azurerm_resource_group.example.name
+  shared_access_key_enabled     = true
+  public_network_access_enabled = true
+  network_rules = {
+    bypass         = ["AzureServices"]
+    default_action = "Allow"
+  }
+}
+*/
+
+/*
 resource "azurerm_service_plan" "example" {
-  location            = azurerm_resource_group.example.location
+  location = azurerm_resource_group.example.location
+  # This will equate to Consumption (Serverless) in portal
   name                = module.naming.app_service_plan.name_unique
   os_type             = "Windows"
   resource_group_name = azurerm_resource_group.example.name
-  sku_name            = "S1"
+  sku_name            = "Y1"
 }
+*/
 
-# This is the module call
 module "test" {
   source = "../../"
 
@@ -63,17 +84,40 @@ module "test" {
 
   enable_telemetry = var.enable_telemetry
 
-  name                = "${module.naming.app_service.name_unique}-windows"
+  name                = "${module.naming.function_app.name_unique}-default"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
 
-  kind    = "webapp"
-  os_type = azurerm_service_plan.example.os_type
-
-  service_plan_resource_id = azurerm_service_plan.example.id
+  kind    = "functionapp"
+  os_type = "Linux"
 
   site_config = {
-
+    ftps_state = "FtpsOnly"
   }
 
+
+  /*
+  # Uses an existing app service plan
+  os_type = azurerm_service_plan.example.os_type
+  service_plan_resource_id = azurerm_service_plan.example.id
+  */
+
+  # Creates a new app service plan
+  create_service_plan = true
+  new_service_plan = {
+    sku_name = "S1"
+  }
+
+  /* 
+  # Uses an existing storage account
+  storage_account_name       = module.avm_res_storage_storageaccount.name
+  storage_account_access_key = module.avm_res_storage_storageaccount.resource.primary_access_key
+  */
+
+  # Uses the avm-res-storage-storageaccount module to create a new storage account within root module
+  function_app_create_storage_account = true
+  function_app_storage_account = {
+    name                = module.naming.storage_account.name_unique
+    resource_group_name = azurerm_resource_group.example.name
+  }
 }
