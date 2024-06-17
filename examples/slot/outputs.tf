8045b9023ec861baa225945003ddab4cc7d035c4
donovm4 <mccoy.donov@gmail.com>
2024-06-17 11:30:58 -0400

    initial commit

---
+++ examples/slot/outputs.tf
@@ -0,0 +1,21 @@
+output "name" {
+  description = "This is the full output for the resource."
+  value       = module.test.name
+}
+
+output "resource" {
+  description = "This is the full output for the resource."
+  sensitive   = true
+  value       = module.test.resource
+}
+
+output "service_plan" {
+  description = "Full output of service plan created"
+  value       = module.test.service_plan
+}
+
+output "storage_account" {
+  description = "Full output of storage account created"
+  sensitive   = true
+  value       = module.test.storage_account
+}
