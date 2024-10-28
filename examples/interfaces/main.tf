a715bef4d2417db8dbfa851e064711409d6987a1
donovm4 <mccoy.donov@gmail.com>
2024-10-27 22:38:41 -0400

    linting fix for vm disk storage type

--- examples/interfaces/main.tf
+++ examples/interfaces/main.tf
@@ -274,7 +274,7 @@ resource "azurerm_windows_virtual_machine" "example" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
   }
   source_image_reference {
     offer     = "WindowsServer"
