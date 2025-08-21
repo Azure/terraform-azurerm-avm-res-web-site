9d22163fe084dae58c8d5cd5aaa3b9d70b86fd4d
Donovan McCoy <mccoy.donov@gmail.com>
2025-08-21 13:50:19 -0400

    pre-commit checks

--- main.function_app.tf
+++ main.function_app.tf
@@ -890,9 +890,9 @@ resource "azurerm_function_app_flex_consumption" "this" {
   # content_share_force_disabled                   = var.content_share_force_disabled
   # daily_memory_time_quota                        = var.daily_memory_time_quota
   enabled                = var.enabled
+  https_only             = var.https_only
   instance_memory_in_mb  = var.instance_memory_in_mb
   maximum_instance_count = var.maximum_instance_count
-  https_only                                     = var.https_only
   # key_vault_reference_identity_id                = var.key_vault_reference_identity_id
   public_network_access_enabled                  = var.public_network_access_enabled
   storage_access_key                             = var.storage_authentication_type == "StorageAccountConnectionString" ? var.storage_account_access_key : null
