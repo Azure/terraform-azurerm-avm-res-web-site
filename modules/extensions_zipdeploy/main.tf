c297ef8721c711b6fa336df25ab3499f40dc0f98
Jared Holgate <jaredholgate@microsoft.com>
2026-03-18 00:16:40 +0000

    feat: update zip deploy to use OneDeploy extension API
    Replace the deprecated extensions/zipdeploy action with the newer extensions/onedeploy API. The body is updated to use the properties wrapper with type=zip as required by the OneDeploy specification.

--- modules/extensions_zipdeploy/main.tf
+++ modules/extensions_zipdeploy/main.tf
@@ -1,10 +1,13 @@
 resource "azapi_resource_action" "this" {
-  action      = "extensions/zipdeploy"
+  action      = "extensions/onedeploy"
   method      = "PUT"
   resource_id = var.parent_id
   type        = local.type
   body = {
-    packageUri = var.zip_deploy_file
+    properties = {
+      packageUri = var.zip_deploy_file
+      type       = "zip"
+    }
   }
 
   lifecycle {
