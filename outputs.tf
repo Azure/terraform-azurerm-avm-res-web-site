a6a8ea99339ffd6c3347d0e172eab8d1dcaa62c1
captainhook <16797541+captainhook@users.noreply.github.com>
2026-08-22 11:54:53 +0100

    Remove sensitive attribute from output variables where it is not needed

--- outputs.tf
+++ outputs.tf
@@ -29,7 +29,6 @@ output "deployment_slots" {
 
 output "identity_principal_id" {
   description = "The system-assigned managed identity principal ID of the resource."
-  sensitive   = true
   value       = try(azapi_resource.this.output.identity.principalId, null)
 }
 
@@ -68,7 +67,6 @@ output "resource" {
 
 output "resource_id" {
   description = "The resource ID of the App Service."
-  sensitive   = true
   value       = azapi_resource.this.id
 }
 
@@ -89,13 +87,11 @@ output "resource_uri" {
 
 output "system_assigned_mi_principal_id" {
   description = "The system-assigned managed identity principal ID."
-  sensitive   = true
   value       = try(azapi_resource.this.output.identity.principalId, null)
 }
 
 output "system_assigned_mi_principal_id_slots" {
   description = "Map of system-assigned managed identity principal IDs for deployment slots."
-  sensitive   = true
   value = {
     for slot_key, slot in module.slot :
     slot_key => slot.identity_principal_id
